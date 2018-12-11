4d6ea6318d8325ab50034b6589d4446602bafe98
frontden <korolenok@strateg.by>
2018-12-11 13:39:39 +0300

    add profile page without responsive

--- src/app/modules/profile/components/profile/profile.component.ts
+++ src/app/modules/profile/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
+import {Performer} from '../../../../domain/models/user.model';
+import {Photo} from '../../../../domain/models/photo.model';
+import {Service} from '../../../../domain/models/service.model';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+    performer: Performer = new Performer();
 
-  ngOnInit() {
-  }
+    @HostBinding('class.app-performer-profile') private isDefaultClassUsed = true;
+
+    constructor() { }
+
+    ngOnInit() {
+
+        this.performer.id = 1;
+
+        this.performer.username = 'Джессика Джонс';
+
+        this.performer.avatar = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWK6h04ZhG5C_KQ5SPw6YedTbr-tppHBJ2INJSR3wIXbRsXKND6g';
+
+        this.performer.address = 'ул.Сторожовская д.6';
+
+        this.performer.description = 'Пара слов о себе';
+
+        this.performer.viewsCount = 27161;
+
+        const photo = new Photo();
+
+        photo.url = 'http://womenshealth.su/wp-content/uploads/2018/02/krasivie_nogti_02.jpg';
+
+        this.performer.portfolio = [photo, photo, photo, photo, photo, photo, photo,
+            photo, photo, photo, photo, photo, photo, photo, photo, photo];
+
+        this.performer.services = [{id: 1, title: 'Маникюр классический', price: 25} as Service,
+            {id: 1, title: 'Маникюр неебический', price: 108} as Service];
+    }
 
 }

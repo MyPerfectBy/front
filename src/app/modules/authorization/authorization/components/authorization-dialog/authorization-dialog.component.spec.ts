b5f1a32863c9cd419eee0d0f38eaabce3e34d86f
kolotovStrateg <kolotovphilipp@gmail.com>
2018-12-11 16:12:01 +0300

    Test for authorization

--- src/app/modules/authorization/authorization/components/authorization-dialog/authorization-dialog.component.spec.ts
+++ src/app/modules/authorization/authorization/components/authorization-dialog/authorization-dialog.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthorizationDialogComponent } from './authorization-dialog.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatDialogRef, MatDividerModule, MatFormFieldModule, MatIconModule, MatInputModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('AuthorizationDialogComponent', () => {
   let component: AuthorizationDialogComponent;
@@ -8,7 +12,21 @@ describe('AuthorizationDialogComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AuthorizationDialogComponent ]
+      declarations: [ AuthorizationDialogComponent ],
+        imports: [
+            NoopAnimationsModule,
+            ReactiveFormsModule,
+            HttpClientModule,
+
+            // material
+            MatFormFieldModule,
+            MatInputModule,
+            MatIconModule,
+            MatDividerModule
+        ],
+        providers: [
+            { provide: MatDialogRef, useValue: {} }
+            ]
     })
     .compileComponents();
   }));

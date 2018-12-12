702a6ed75e0b40f6d066182fb4828a3138a23777
yaahor <yaahor@gmail.com>
2018-12-12 15:16:45 +0300

    AuthorizationDialogComponent: test fix

--- src/app/modules/authorization/services/authorization.service.spec.ts
+++ src/app/modules/authorization/services/authorization.service.spec.ts
@@ -1,12 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { AuthorizationService } from './authorization.service';
+import {AuthorizationService} from './authorization.service';
+import {HttpClient} from '@angular/common/http';
 
 describe('AuthorizationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: AuthorizationService = TestBed.get(AuthorizationService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => TestBed.configureTestingModule({
+        providers: [
+            {
+                provide: HttpClient,
+                useValue: {}
+            }
+        ]
+    }));
+
+    it('should be created', () => {
+
+        const service: AuthorizationService = TestBed.get(AuthorizationService);
+
+        expect(service).toBeTruthy();
+    });
 });

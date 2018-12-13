acb771198e99b95d73a7865401535c874b10a3d9
yaahor <yaahor@gmail.com>
2018-12-13 12:56:53 +0300

    promo.component authorization fix

--- src/app/modules/promo/components/promo/promo.component.ts
+++ src/app/modules/promo/components/promo/promo.component.ts
@@ -1,8 +1,13 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 import {MatDialog} from '@angular/material';
-import {Subscription} from 'rxjs';
 
+// rxjs
+import {Observable, Subject, Subscription} from 'rxjs';
+import {delay, filter, switchMap, takeUntil, tap} from 'rxjs/operators';
+
+// models
+import {User} from '../../../../domain/models/user.model';
 // components
 import {
     AuthorizationDialogComponent
@@ -10,36 +15,30 @@ import {
 import {
     PerformerRegistrationDialogComponent
 } from '../../../registration/components/performer-registration-dialog/performer-registration-dialog.component';
-
-
 // services
-import {
-    AuthorizationService
-} from '../../../authorization/services/authorization.service';
-import {delay} from 'rxjs/operators';
-
-
-
+import {AuthorizationService} from '../../../authorization/services/authorization.service';
 
 @Component({
     selector: 'app-promo',
     templateUrl: './promo.component.html',
     styleUrls: ['./promo.component.scss']
 })
-export class PromoComponent implements OnInit {
+export class PromoComponent implements OnInit, OnDestroy {
 
     config: any;
+
     fullpage_api: any;
+
+    private destroy$ = new Subject();
 
     private querySubscription: Subscription;
 
-    constructor(private dialogService: MatDialog, private route: ActivatedRoute, private authorizationService: AuthorizationService) {
+    constructor(private dialogService: MatDialog, private route: ActivatedRoute, private router: Router,
+                private authorizationService: AuthorizationService) {
 
         this.config = {
-
             menu: '#menu',
             navigation: true,
-
         };
 
         this.querySubscription = route.queryParams.subscribe(
@@ -62,20 +61,36 @@ export class PromoComponent implements OnInit {
     }
 
     openAuthorizationDialog(): void {
-        const config = {};
 
-        const dialogRef = this.dialogService.open(AuthorizationDialogComponent, config);
+        const dialogRef = this.dialogService.open(AuthorizationDialogComponent);
 
-        dialogRef.afterClosed().subscribe(() => {
-        });
+        dialogRef.afterClosed()
+            .pipe(
+                switchMap((): Observable<User> => this.authorizationService.getUser()),
+                filter((user: User): boolean => !!user),
+                tap((user: User): void => {
+
+                    this.router.navigate([user.username]);
+                }),
+                takeUntil(this.destroy$)
+            )
+            .subscribe();
     }
 
     getRef(fullPageRef) {
         this.fullpage_api = fullPageRef;
     }
 
+// lifecycle hooks -------------------------------------------------------------------------------------------------------------------------
 
     ngOnInit() {
+    }
+
+    ngOnDestroy(): void {
+
+        this.destroy$.next();
+
+        this.destroy$.unsubscribe();
     }
 
 }
